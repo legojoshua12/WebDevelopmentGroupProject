b23f05750ae4c6ee557135bf37a3f9c1d5a19371
Joshua Crews <legojoshua12@gmail.com>
2021-01-02 23:11:33 -0800

    Hooked up front end with backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Covid_Form from "./backend/AddCases"
+import DisplayCovid from "./backend/DisplayCases.js"
+import Covid_Update from "./backend/CovidUpdate"
+import DeleteCase from "./backend/DeleteCase"
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+        <center><h1>On-Line COVID database using REACT</h1></center>
+           <br/>
+            
+            <nav className="navbar navbar-expand-lg navbar-light bg-success">
+            <Link to="/" className="navbar-brand"><h4>Add COVID information</h4></Link>
+            <Link to="/edit/:id" className="navbar-brand"><h4>Update COVID information</h4></Link>
+            <Link to="/Delete/:id" className="navbar-brand"><h4>Delete COVID information</h4></Link>
+            <Link to="/DisplayCovid" className="navbar-brand"><h4>Display all cases</h4></Link>
+            </nav>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <br/>
+          <Route path="/" component={Covid_Form} />
+          <Route path="/edit/:id" component={Covid_Update} />
+          <Route path="/Delete/:id" component={DeleteCase} />
+          <Route path="/DisplayCovid" component={DisplayCovid} /> 
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
